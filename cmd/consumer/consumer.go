31e1835f6febd284c2ae30954fde6c0bdc9290e1
Esterfano Lopes <esterfano.lopes@gmail.com>
2022-07-28 17:18:57 +0200

    config consumer setup

--- cmd/consumer/consumer.go
+++ cmd/consumer/consumer.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"os/signal"
 	"strings"
@@ -12,43 +11,71 @@ import (
 	"github.com/Shopify/sarama"
 )
 
-func main() {
-	config.Init()
-	// Create new consumer
-	master, err := sarama.NewConsumer(config.Cfg.Addresses, config.Cfg.SaramaConfig)
+type Consumer struct {
+	Client           sarama.Consumer
+	ErrorChan        chan *sarama.ConsumerError
+	MessageChan      chan *sarama.ConsumerMessage
+	DoneChan         chan struct{}
+	Signals          chan os.Signal
+	NumberOfMessages int
+}
+
+func InitConsumerConfig() Consumer {
+	client, err := sarama.NewConsumer(config.Cfg.Addresses, config.Cfg.SaramaConfig)
 	if err != nil {
 		config.Cfg.Logger.Println(err)
 	}
 
+	consumers := make(chan *sarama.ConsumerMessage)
+	errors := make(chan *sarama.ConsumerError)
+	done := make(chan struct{})
+	signals := make(chan os.Signal, 1)
+
+	return Consumer{
+		Client:      client,
+		MessageChan: consumers,
+		ErrorChan:   errors,
+		DoneChan:    done,
+		Signals:     signals,
+	}
+}
+
+func main() {
+	config.Init()
+	// Create new consumer
+	c := InitConsumerConfig()
 	defer func() {
-		if err := master.Close(); err != nil {
+		if err := c.Client.Close(); err != nil {
 			config.Cfg.Logger.Println(err)
 		}
+
+		close(c.DoneChan)
+		close(c.ErrorChan)
+		close(c.Signals)
 	}()
 
-	topics, _ := master.Topics()
+	// Get client topics
+	topics, err := c.Client.Topics()
+	if err != nil {
+		config.Cfg.Logger.Println(err)
+	}
 
-	consumer, errors := consume(topics, master)
-
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
-
-	// Count how many message processed
-	msgCount := 0
+	consumeTopics(c, topics)
+	signal.Notify(c.Signals, os.Interrupt)
 
 	// Get signnal for finish
 	doneCh := make(chan struct{})
 	go func() {
 		for {
 			select {
-			case msg := <-consumer:
-				msgCount++
+			case msg := <-c.MessageChan:
+				c.NumberOfMessages++
 				config.Cfg.Logger.Println("Received messages", string(msg.Key), string(msg.Value))
-			case consumerError := <-errors:
-				msgCount++
+			case consumerError := <-c.ErrorChan:
+				c.NumberOfMessages++
 				config.Cfg.Logger.Println("Received consumerError ", string(consumerError.Topic), string(consumerError.Partition), consumerError.Err)
 				doneCh <- struct{}{}
-			case <-signals:
+			case <-c.Signals:
 				config.Cfg.Logger.Println("Interrupt is detected")
 				doneCh <- struct{}{}
 			}
@@ -56,44 +83,43 @@ func main() {
 	}()
 
 	<-doneCh
-	config.Cfg.Logger.Println("Processed", msgCount, "messages")
+	config.Cfg.Logger.Println("Processed", c.NumberOfMessages, "messages")
 
 }
 
-func consume(topics []string, master sarama.Consumer) (chan *sarama.ConsumerMessage, chan *sarama.ConsumerError) {
-	consumers := make(chan *sarama.ConsumerMessage)
-	errors := make(chan *sarama.ConsumerError)
+func consumeTopics(c Consumer, topics []string) {
 	for _, topic := range topics {
 		if strings.Contains(topic, "__consumer_offsets") {
 			continue
 		}
-		partitions, _ := master.Partitions(topic)
-		// this only consumes partition no 1, you would probably want to consume all partitions
-		consumer, err := master.ConsumePartition(topic, partitions[0], sarama.OffsetOldest)
-		if nil != err {
-			fmt.Printf("Topic %v Partitions: %v", topic, partitions)
-			config.Cfg.Logger.Println(err)
-		}
-		config.Cfg.Logger.Println(" Start consuming topic ", topic)
-		go func(topic string, consumer sarama.PartitionConsumer) {
-			for {
-				select {
-				case consumerError := <-consumer.Errors():
-					errors <- consumerError
-					config.Cfg.Logger.Println("consumerError: ", consumerError.Err)
+		partitions, _ := c.Client.Partitions(topic)
 
-				case msg := <-consumer.Messages():
-					consumers <- msg
-
-					r := data.Record{}
-
-					json.Unmarshal(msg.Value, &r)
-
-					config.Cfg.Logger.Println("Got message on topic ", topic, r)
-				}
+		for _, partition := range partitions {
+			partitionConsumer, err := c.Client.ConsumePartition(topic, partition, sarama.OffsetOldest)
+			if nil != err {
+				config.Cfg.Logger.Printf("Topic %v Partition: %v", topic, partition)
+				config.Cfg.Logger.Println(err)
 			}
-		}(topic, consumer)
+			config.Cfg.Logger.Println(" Start consuming topic ", topic)
+			go consumePartition(c, topic, partitionConsumer)
+		}
 	}
+}
 
-	return consumers, errors
+func consumePartition(c Consumer, topic string, consumer sarama.PartitionConsumer) {
+	for {
+		select {
+		case consumerError := <-consumer.Errors():
+			c.ErrorChan <- consumerError
+			config.Cfg.Logger.Println("consumerError: ", consumerError.Err)
+
+		case msg := <-consumer.Messages():
+			c.MessageChan <- msg
+
+			r := data.Record{}
+			json.Unmarshal(msg.Value, &r)
+
+			config.Cfg.Logger.Println("Got message on topic ", topic, r)
+		}
+	}
 }
